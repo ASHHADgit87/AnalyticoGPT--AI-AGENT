e8596d17fd70c3b55c25daf6141dd0a9263a6f56
Muhammad Ashhadullah Zaheer <ashhadullahzahir@gmail.com>
2026-06-25 13:41:58 +0500

    Made Ui realistic in charts page

--- ui/charts_page.py
+++ ui/charts_page.py
@@ -4,7 +4,7 @@ import os
 
 def render_charts_layout():
     st.markdown(
-        '<div class="main-header">📊 PIPELINE GRAPHICAL INTERFACES</div>',
+        '<div class="main-header"> PIPELINE GRAPHICAL INTERFACES</div>',
         unsafe_allow_html=True,
     )
 
