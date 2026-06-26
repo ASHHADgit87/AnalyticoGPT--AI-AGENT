1d05fd22f87533571baa87f55a2f43c00c4bfa08
Muhammad Ashhadullah Zaheer <ashhadullahzahir@gmail.com>
2026-06-26 19:26:04 +0500

    Adjusted size

--- streamlit_app.py
+++ streamlit_app.py
@@ -272,10 +272,10 @@ st.markdown(
 
         [data-testid="stSidebar"] [data-testid="stVerticalBlock"] > div:first-child img,
         [data-testid="stSidebar"] [data-testid="stImage"] img {{
-            height: 2.2rem !important;
+            height: 2rem !important;
             margin-top: 0.78rem;
             width: auto !important;
-            max-height: 2.2rem !important;
+            max-height: 2rem !important;
             display: block !important;
             object-fit: contain !important;
         }}
